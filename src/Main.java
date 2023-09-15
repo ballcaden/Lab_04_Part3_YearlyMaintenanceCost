e82b3c9ed49c1c306d98bac3942ba0f88fd86d85
ballcaden <ballcaden@gmail.com>
2023-09-15 09:32:31 -0400

    Initial

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    Scanner scan = new Scanner(System.in);
+    double springMaintenanceCost;
+    double summerMaintenanceCost;
+    double fallMaintenanceCost;
+    double winterMaintenanceCost;
+    double maintenanceCost;
+
+    System.out.println("Enter your spring maintenance cost: ");
+    springMaintenanceCost = scan.nextDouble();
+    System.out.println("Enter your summer maintenance cost: ");
+    summerMaintenanceCost = scan.nextDouble();
+    System.out.println("Enter your fall maintenance cost: ");
+    fallMaintenanceCost = scan.nextDouble();
+    System.out.println("Enter your winter maintenance cost: ");
+    winterMaintenanceCost = scan.nextDouble();
+    maintenanceCost = springMaintenanceCost + summerMaintenanceCost + fallMaintenanceCost + winterMaintenanceCost;
+    System.out.println("Your yearly maintenance cost is: " + maintenanceCost + "$");
+
     }
 }
